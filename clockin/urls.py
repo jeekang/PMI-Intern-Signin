91d79fd314630e14c53f081908234b98fe721178
Sam Shiao <srshiao@gmail.com>
2017-09-14 23:04:33 -0400

    Refactoring
    person_list.html -> active_work_sessions.html
    new_person.html -> new_work_session.html
    item_edit.html -> end_work_session.html
    datefilter.html -> all_work_sessions.html
    person_confirm_delete -> delete_work_session.html
    new_record -> admin_add_work_session
    
    ogdb -> timesheet
    
    Updated urls.py and views.py accordingly

--- clockin/urls.py
+++ clockin/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     #url(r'^(?P<work_id>[0-9]+)/$', views.detail, name='detail'),
     url(r'^home/$', views.work_list, name= 'home'),
     url(r'^start_shift/$', views.add_new, name= 'add'),
-    url(r'^edit/(?P<work_id>\d+)/$', views.clockout, name='item_edit'),
+    url(r'^edit/(?P<work_id>\d+)/$', views.clockout, name='end_work_session'),
     url(r'^delete/(?P<pk>\d+)/$', views.workDelete.as_view(model = Work), name="work_delete"),
     #url(r'^test/$', views.WorkListView.as_view(), name="person_list_2"),
     url(r'^adminhome/$', views.AdminView, name= 'adminhome'),
